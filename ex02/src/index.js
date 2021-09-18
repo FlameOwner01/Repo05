eb09449c120b7494979ffcc870ecf7bbe614b1bf
FlameOwner <73409497+FlameOwner01@users.noreply.github.com>
2021-09-18 09:54:11 +0200

    first commit

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    var myLink1 = '<a href="http//:www.example.com" target="_blank">Link</a>'
+    var myLink = myLink1.toString();
+    // Only change code above this line
+
+    return myLink;
+}
+
+console.log(main());
+module.exports = main;
